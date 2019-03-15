0d228e5b719fd4fa96356e84f4c75a86945eb920
GlebGlushko <gleb.glushko10@gmail.com>
2019-03-15 17:28:38 +0200

    fixed some bugs, add tobase64 method, add more texts

--- L1/main.py
+++ L1/main.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plot
 import numpy as np
 import math
-import  os
+import os
+
 files_paths = {'vinnyk.txt'}
 alphabet = list('абвгґдеєжзиіїйклмнопрстуфхцчшщьюя')
-#alphabet_upper = list('АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ')
 alphabet_size = len(alphabet)
 
 
@@ -14,10 +14,14 @@ def display_plot_per_letter(freq):
     plot.bar(x=alphabet, height=freq)
     plot.show()
 
-def calculate_frequency(text, some_size):
-    print("file_size = ", some_size)
-    text = str(text).lower()
+
+def calculate_frequency(text_in):
+    # print("file_size = ", some_size)
+    text = ""
+    for x in text_in:
+        text += str(x)
     letters_count = 0
+    symbols_count = 0
     freq = np.zeros(alphabet_size, dtype='int')
     for symb in text:
         pos = -1
@@ -28,22 +32,35 @@ def calculate_frequency(text, some_size):
         if pos != -1:
             freq[pos] += 1
             letters_count += 1
-    #display_plot_per_letter(freq)
-    print("letters count = ", letters_count)
-    calculate_enthropy(freq, letters_count)
+        symbols_count += 1
+    return (freq, letters_count, symbols_count)
 
 
-def calculate_enthropy(freq, letters_count):
+def calculate_entropy(freq, letters_count):
     entropy = 0
-    print("x = {0}", freq)
     for x in freq:
         if x > 0:
-            entropy -= x/letters_count * math.log2(x/letters_count)
-            # print('%.2f'% (-x/letters_count * math.log2(x/letters_count)), end=' ')
-    print("entropy = ",entropy)
+            entropy -= x / letters_count * math.log2(x / letters_count)
+    return entropy
+
+
+def compare_file_size(file_size, entropy, letters_count, symbols_count):
+    print("entropy = {1},\nletters_count = {2},\ntotal_symbols_count = {3},\nfile size = {0},".
+          format(file_size, entropy, letters_count, symbols_count))
+    print("predicted_file_size = {0},".format(entropy * symbols_count/8))
+    print("average_size(2bytes=1ukr_letter&enter,1bytes=other_symbols) = {0},"
+          .format(entropy * (letters_count/4 + (symbols_count-letters_count)/8)))
+
 
 if __name__ == '__main__':
     for path in files_paths:
         print(path)
         file = open(path, "r", encoding='utf-8', errors='ignore')
-        calculate_frequency(file.readlines(), os.stat(path).st_size)
+        entropy_data = calculate_frequency(file.readlines())
+        compare_file_size(os.stat(path).st_size, calculate_entropy(entropy_data[0], entropy_data[1]), entropy_data[1],
+                          entropy_data[2])
+        display_plot_per_letter(calculate_entropy(entropy_data[0], entropy_data[1]))
+
+
+
+
