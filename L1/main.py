efa4c2e36a1591038ad28fd9e6cbb5aa186ff682
GlebGlushko <gleb.glushko10@gmail.com>
2019-03-18 15:04:27 +0200

    add plot for different file archivers, add report

--- L1/main.py
+++ L1/main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 import os
@@ -9,9 +9,9 @@ files_paths = {'docker.txt'}
 def display_plot_per_letter(freq, alphabet):
     # plot.subplot(212)
 
-    plot.figure(figsize=(10, 5))
-    plot.bar(x=alphabet, height=freq)
-    plot.show()
+    plt.figure(figsize=(10, 5))
+    plt.bar(x=alphabet, height=freq)
+    plt.show()
 
 
 def calculate_frequency(text_in, alphabet):
@@ -52,6 +52,51 @@ def compare_file_size(file_size, entropy, letters_count, symbols_count):
     #       .format(entropy * (letters_count/4 + (symbols_count-letters_count)/8)))
 
 
+archive_name = ('orginal', 'rar', 'zip', '7-zip','PeaZip')
+archive_size_docker = (11675, 3809, 3961, 3643, 3953)
+archive_size_weed = (7381, 2551, 2616, 2480, 2556)
+archive_size_vinnyk = (1464, 533, 533, 555, 542)
+fn = ('docker', 'weed', 'vinnyk')
+ao = (11675, 7381, 1464)
+ar = (3809, 2551, 533)
+az = (3961, 2616, 533)
+a7 = (3643, 2480, 555)
+ap = (3953, 2556, 542)
+
+
+def display_plot_per_archive():
+
+    fig, ax = plt.subplots()
+    index = np.arange(3, 10, 3)
+    bar_width = 0.35
+    opacity = 0.8
+
+    rects1 = plt.bar(index - 2*bar_width, ao, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     label='originl')
+
+    rects2 = plt.bar(index - bar_width, ar, bar_width,
+                     alpha=opacity,
+                     color='g',
+                     label='rar')
+    rects3 = plt.bar(index, az, bar_width,
+                     alpha=opacity,
+                     color='r',
+                     label='zip')
+    rects4 = plt.bar(index + bar_width, a7, bar_width,
+                      alpha=opacity,
+                      color='y',
+                      label='7-zip')
+    rects5 = plt.bar(index + 2*bar_width, ap, bar_width,
+                      alpha=opacity,
+                      color='black',
+                      label='peazip')
+
+    plt.xticks(index, fn)
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
     for path in files_paths:
         print(path)
@@ -61,8 +106,7 @@ if __name__ == '__main__':
         entropy_data = calculate_frequency(file.readlines(), alphabet)
         compare_file_size(os.stat(path).st_size, calculate_entropy(entropy_data[0], entropy_data[1]), entropy_data[1],
                           entropy_data[2])
-        display_plot_per_letter(entropy_data[0], alphabet)
-
-
+        # display_plot_per_letter(entropy_data[0], alphabet)
+        display_plot_per_archive()
 
 
--- L1/tobase64.py
+++ L1/tobase64.py
@@ -3,44 +3,47 @@ from main import *
 import sys
 
 b64s = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+
 def tobase64(s):
     bs = ''
     res = ''
     for x in s:
-
-        # print(str(bin(int.from_bytes(bytearray(x,'utf-8'), byteorder='big'))), ' this is = ',x,'end')
-        bs+= str(bin(int.from_bytes( bytearray(x,'utf-8'), byteorder='big')))[2:].zfill(8)
-        while len(bs)>=6:
-            res += b64s[int(bs[:6],2)]
+        bs += str(bin(int.from_bytes(bytearray(x, 'utf-8'), byteorder='big')))[2:].zfill(8)
+        while len(bs) >= 6:
+            res += b64s[int(bs[:6], 2)]
             bs = bs[6:]
+    kol =0
     if len(bs) > 0:
-        while len(bs)<6:
+        while len(bs) < 6:
+            kol += 1
             bs += '0'
-        res += b64s[int(bs[:6],2)]
-
+        res += b64s[int(bs[:6], 2)]
+    res += '=' * int(kol/2)
     return res
 
 
 if __name__ == '__main__':
-    #print("ukraine")
-    path = "lab.txt"
+    path = "weed.txt"
     file = open(path, "r", encoding='utf-8').readlines()
     txt = ''
     for x in file:
         txt += x
+    # txt = "Слава Україні!!"
     txt.encode(encoding='utf-8')
     libtxt64 = str(base64.b64encode(bytes(txt,encoding='utf-8')))[2:-1]
     txt64 = tobase64(txt)
-    print(txt64)
-    print(libtxt64)
+    # print('input: ', txt)
+    # print('my result:  ', txt64)
+    # print('lib result: ', libtxt64)
+    print(path)
     print(txt64 == libtxt64)
 
     alphabet = list('abcdefghijklmnopqrstuvwxyz0123456789+/')
     alphabet_size = len(alphabet)
     entropy_data = calculate_frequency(txt64, alphabet)
     compare_file_size(sys.getsizeof(txt64), calculate_entropy(entropy_data[0], entropy_data[1]), entropy_data[1],
-                      entropy_data[2])
+                    entropy_data[2])
 
     display_plot_per_letter(entropy_data[0], alphabet)
 
-# 00100000  0000
