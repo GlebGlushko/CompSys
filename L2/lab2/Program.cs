c71e7cc91bfc163f50a10f004ae3b5794456a493
Глеб Глушко <gleb.glushko10@gmail.com>
2019-03-26 10:01:21 +0200

    add report

--- L2/lab2/Program.cs
+++ L2/lab2/Program.cs
@@ -7,21 +7,21 @@ namespace Lab2
      
         static void Main(string[] args)
         {
-            Int16 x, y;
-            Console.WriteLine("Enter 16 bits signed multiplicand:");
-            x = Int16.Parse(Console.ReadLine());
-            Console.WriteLine("Enter 16 bits signed multiplier:");
-            y = Int16.Parse(Console.ReadLine());
-            Multiply m = new Multiply(x, y);
-            m.Do();
+            //Int16 x, y;
+            //Console.WriteLine("Enter 16 bits signed multiplicand:");
+            //x = Int16.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter 16 bits signed multiplier:");
+            //y = Int16.Parse(Console.ReadLine());
+            //Multiply m = new Multiply(x, y);
+            //m.Do();
 
 
-            Console.WriteLine("Enter 16 bits signed dividend:");
-            x = Int16.Parse(Console.ReadLine());
-            Console.WriteLine("Enter 16 bits signed divisor:");
-            y = Int16.Parse(Console.ReadLine());
-            Divide d = new Divide(x, y);
-            d.Do();
+            //Console.WriteLine("Enter 16 bits signed dividend:");
+            //x = Int16.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter 16 bits signed divisor:");
+            //y = Int16.Parse(Console.ReadLine());
+            //Divide d = new Divide(x, y);
+            //d.Do();
 
             float a, b;
             Console.WriteLine("Enter first float signed value:");
